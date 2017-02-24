79563096a50da8bfc8d5ffb099bbcc9de10a6f10
Josh Bruggeman <joshrbruggeman@gmail.com>
2017-02-24 12:06:00 -0600

    database

---
+++ db.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+  // Hook mongoose configuration to the db variable
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    console.log("connected to mongoose");
+  });
+
+  var articleSchema = mongoose.Schema({
+      title: String,
+      link: String
+  });
+}
